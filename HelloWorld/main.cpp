443d1d810005a2aff1b45ec61fc98ac7263a9f82
840788021@qq.com <gh751070352>
2020-10-28 19:48:44 +0800

    Helloworld: 修改显示尺寸

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -7,7 +7,11 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     
     QDialog w;
+    w.resize(400,300);
+
     QLabel label(&w);
+    label.move(120,120);
+
     label.setText("Hello World! 你好 QT!");
     w.show();
     // 让 QApplication 对象进入事件循环
